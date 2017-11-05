2d60d5cf9eb14450aaecd76afba47f958967615a
Dhiraj Borade <dhirajborade@gmail.com>
2017-11-05 15:22:15 -0500

    Test Cases from Harshit

--- src/cop5556fa17/TypeCheckTest.java
+++ src/cop5556fa17/TypeCheckTest.java
@@ -1,34 +1,12 @@
 package cop5556fa17;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import static org.junit.Assert.*;
 
+import cop5556fa17.TypeCheckVisitor.SemanticException;
 import cop5556fa17.AST.ASTNode;
 import cop5556fa17.AST.ASTVisitor;
-import cop5556fa17.AST.Declaration_Image;
-import cop5556fa17.AST.Declaration_SourceSink;
-import cop5556fa17.AST.Declaration_Variable;
-import cop5556fa17.AST.Expression;
-import cop5556fa17.AST.Expression_FunctionAppWithExprArg;
-import cop5556fa17.AST.Expression_IntLit;
-import cop5556fa17.AST.Expression_PixelSelector;
-import cop5556fa17.AST.Expression_PredefinedName;
-import cop5556fa17.AST.Expression_Unary;
-import cop5556fa17.AST.Index;
-import cop5556fa17.AST.LHS;
-import cop5556fa17.AST.Program;
-import cop5556fa17.AST.Source_CommandLineParam;
-import cop5556fa17.AST.Source_StringLiteral;
-import cop5556fa17.AST.Statement_Out;
-import cop5556fa17.AST.Statement_Assign;
-import cop5556fa17.Parser.SyntaxException;
-import cop5556fa17.Scanner.Kind;
-import cop5556fa17.Scanner.LexicalException;
-import cop5556fa17.Scanner.Token;
-import cop5556fa17.TypeCheckVisitor.SemanticException;
-
-import static cop5556fa17.Scanner.Kind.*;
 
 public class TypeCheckTest {
 
@@ -38,13 +16,13 @@ public class TypeCheckTest {
 
 	// To make it easy to print objects and turn this output on and off
 	static final boolean doPrint = true;
+
 	private void show(Object input) {
 		if (doPrint) {
 			System.out.println(input.toString());
 		}
 	}
-	
-	
+
 	/**
 	 * Scans, parses, and type checks given input String.
 	 * 
@@ -74,7 +52,7 @@ public class TypeCheckTest {
 	 */
 	@Test
 	public void testSmallest() throws Exception {
-		String input = "n"; //Smallest legal program, only has a name
+		String input = "n"; // Smallest legal program, only has a name
 		show(input); // Display the input
 		Scanner scanner = new Scanner(input).scan(); // Create a Scanner and
 														// initialize it
@@ -87,30 +65,28 @@ public class TypeCheckTest {
 		show(ast);
 	}
 
-
-
-	
 	/**
 	 * This test should pass with a fully implemented assignment
+	 * 
 	 * @throws Exception
 	 */
-	 @Test
-	 public void testDec1() throws Exception {
-	 String input = "prog int k = 42;";
-	 typeCheck(input);
-	 }
-	 
-	 /**
-	  * This program does not declare k. The TypeCheckVisitor should
-	  * throw a SemanticException in a fully implemented assignment.
-	  * @throws Exception
-	  */
-	 @Test
-	 public void testUndec() throws Exception {
-	 String input = "prog k = 42;";
-	 thrown.expect(SemanticException.class);
-	 typeCheck(input);
-	 }
+	@Test
+	public void testDec1() throws Exception {
+		String input = "prog int k = 42;";
+		typeCheck(input);
+	}
 
+	/**
+	 * This program does not declare k. The TypeCheckVisitor should throw a
+	 * SemanticException in a fully implemented assignment.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testUndec() throws Exception {
+		String input = "prog k = 42;";
+		thrown.expect(SemanticException.class);
+		typeCheck(input);
+	}
 
 }
